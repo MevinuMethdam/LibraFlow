9fb29dea9bc8013eeb01a444da726ed0ef874368
prjcts-Titan <nipun.navodya.ise@gmail.com>
2026-09-20 17:45:32 +0530

    feat: add user login API
    Co-authored-by: MevinuMethdam <mevinumethdam@yahoo.com>

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,15 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', require('./src/routes/userRoutes'));
-app.use('/api/books', require('./src/routes/bookRoutes'));
+// මෙතන අමතරව තිබ්බ '/src' කෑල්ල අයින් කරලා තියෙන්නේ
+//app.use('/api/users', require('./routes/userRoutes'));
+//app.use('/api/books', require('./routes/bookRoutes'));
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('MongoDB Connected successfully');
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
